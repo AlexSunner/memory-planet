0aaae80090c2fddf081412ba2c4ec8487f89fa11
AlexSunner <>
2023-11-17 15:15:34 +0000

    Add basic CSS styling to body and wrapper

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let moves = document.getElementById("moves-count");
+let timeValue = document.getElementById("time-count");
+let startButton = document.getElementById("start-game");
+let stopButton = document.getElementById("stop-game");
