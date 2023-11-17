ab854ae74c559ff86d161ff754a194ee5802f09d
AlexSunner <>
2023-11-17 15:48:04 +0000

    Add time generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*
-* Basic JavaScript structure
+* create variables and connect to the ID's and classes
 */
 let moves = document.getElementById("moves-count");
 let timeValue = document.getElementById("time-count");
@@ -13,3 +13,32 @@ let interval;
 let firstCard = false;
 let secondCard = false;
 
+/*
+* Array of game icons
+*/
+let items = [
+    { name: "crab", image: "crab.png" },
+    { name: "goose", image: "goose.png" },
+    { name: "lion", image: "lion.png" },
+    { name: "penguin", image: "penguin.png" },
+    { name: "ram", image: "ram.png" },
+    { name: "squirrel", image: "squirrel.png" }
+];
+
+/*
+* Game time and moves count
+*/
+let seconds = 0,
+minutes = 0;
+let movesCount = 0,
+winCount = 0;
+let timeGenerator = () => {
+    seconds += 1;
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+let minutesValues = minutes < 10 ? `0${minutes}` : minutes;
+timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
