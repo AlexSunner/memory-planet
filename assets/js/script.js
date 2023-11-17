3f8b167677c608ea810cd260916e15371ce0119e
AlexSunner <>
2023-11-17 15:35:08 +0000

    Add game images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
+/*
+* Basic JavaScript structure
+*/
 let moves = document.getElementById("moves-count");
 let timeValue = document.getElementById("time-count");
 let startButton = document.getElementById("start-game");
 let stopButton = document.getElementById("stop-game");
+let gameContainer = document.querySelector(".game-container");
+let result = document.getElementById("result");
+let controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
